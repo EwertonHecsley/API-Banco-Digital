e6bd6788a4230bb3b4254ec25bd9aa7c61678e58
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-06-08 11:41:10 -0300

    Finalizando API

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,115 +1,102 @@
-const bancoDeDados = require('../bancodedados');
+const { contas, depositos, saques, transferencias } = require('../bancodedados');
 const { format } = require('date-fns');
+const { procurarConta } = require('../utils/funcoeAuxiliares');
 
-const listaConta = (req, res) => {
-    return res.status(200).json(bancoDeDados.contas);
+const listaConta = (_req, res) => {
+    return res.status(200).json(contas);
 };
 
 const criarConta = (req, res) => {
     const corpo = req.body;
 
-    const ultimaConta = bancoDeDados.contas[bancoDeDados.contas.length - 1];
+    const ultimaConta = contas[contas.length - 1];
 
     const novaConta = {
-        "numero": bancoDeDados.contas.length === 0 ? "1" : JSON.stringify(Number(ultimaConta.numero) + 1),
-        "saldo": 0,
-        ...corpo
+        numero: contas.length === 0 ? "1" : JSON.stringify(Number(ultimaConta.numero) + 1),
+        saldo: 0,
+        usuario: {
+            ...corpo
+        }
     };
 
-    bancoDeDados.contas.push(novaConta);
+    contas.push(novaConta);
 
     return res.status(201).json(novaConta);
 };
 
-const atualzaConta = (req, res) => {
+const atualzaConta = async (req, res) => {
     const corpo = req.body;
     const { numeroConta } = req.params;
-    const usuario = bancoDeDados.contas.find((user) => user.numero === numeroConta);
+
+    const usuarioConta = await procurarConta(numeroConta, contas);
 
     const keys = Object.keys(corpo);
 
     for (let i of keys) {
-        usuario[i] = corpo[i];
+        usuarioConta.usuario[i] = corpo[i];
     };
 
-    return res.status(200).json({ "menssagem": "Conta atualizada com sucesso!" });
+    return res.status(200).json({ mensagem: "Conta atualizada com sucesso!" });
 
 };
 
-const excluirConta = (req, res) => {
+const excluirConta = async (req, res) => {
     const { numeroConta } = req.params;
 
-    const conta = bancoDeDados.contas.find((conta) => conta.numero === numeroConta);
+    const conta = await procurarConta(numeroConta, contas);
 
-    try {
-        if (isNaN(numeroConta)) {
-            throw new Error("Número de conta inválido");
-        };
+    const index = contas.indexOf(conta);
 
-        if (!conta) {
-            throw new Error("Número de conta inexistente");
-        }
+    contas.splice(index, 1);
 
-        if (conta.saldo > 0) {
-            throw new Error("Conta com saldo maior que R$ 0,00, não pode ser excluída!");
-        };
-
-        const index = bancoDeDados.contas.indexOf(conta);
-
-        bancoDeDados.contas.splice(index, 1);
-
-        return res.status(200).json({ "mensagem": "Conta excluída com sucesso!" });
-
-    } catch (error) {
-        return res.status(400).json({ "mensagem": error.message });
-    };
+    return res.status(200).json({ mensagem: "Conta excluída com sucesso!" });
 };
 
-const depositar = (req, res) => {
-    const { numero, valor, data } = req.body;
+const depositar = async (req, res) => {
+    const { numero, valor } = req.body;
 
-    const conta = bancoDeDados.contas.find((cont) => cont.numero === numero);
+    const conta = await procurarConta(numero, contas);
 
-    const dataInput = new Date(data);
+    const data = new Date();
 
     conta.saldo += Number(valor);
 
     const registro = {
-        "data": format(dataInput, 'yyyy-MM-dd HH:mm:ss'),
-        "numero_conta": numero,
-        "valor": Number(valor)
+        data: format(data, 'yyyy-MM-dd HH:mm:ss'),
+        numero_conta: numero,
+        valor: Number(valor)
     };
 
-    bancoDeDados.depositos.push(registro);
+    depositos.push(registro);
 
-    return res.status(200).json(({ "mensagem": "Depósito realizado com sucesso!" }));
+    return res.status(200).json(({ mensagem: "Depósito realizado com sucesso!" }));
 };
 
-const sacar = (req, res) => {
+const sacar = async (req, res) => {
     const { numero, valor } = req.body;
 
-    const conta = bancoDeDados.contas.find((cont) => cont.numero === numero);
+    const conta = await procurarConta(numero, contas);
 
     const data = new Date();
 
     conta.saldo -= Number(valor);
 
     const registro = {
-        "data": format(data, 'yyyy-MM-dd HH:mm:ss'),
-        "numero_conta": numero,
-        "valor": Number(valor)
+        data: format(data, 'yyyy-MM-dd HH:mm:ss'),
+        numero_conta: numero,
+        valor: Number(valor)
     };
 
-    bancoDeDados.saques.push(registro);
+    saques.push(registro);
 
-    return res.status(200).json({ "mensagem": "Saque realiazado com sucesso!" });
+    return res.status(200).json({ mensagem: "Saque realiazado com sucesso!" });
 };
 
-const transferencia = (req, res) => {
+const transferencia = async (req, res) => {
     const { numero_conta_origem, numero_conta_destino, valor } = req.body;
 
-    const userOrigem = bancoDeDados.contas.find((user) => user.numero === numero_conta_origem);
-    const userDestino = bancoDeDados.contas.find((user) => user.numero === numero_conta_destino);
+    const userOrigem = await procurarConta(numero_conta_origem, contas);
+    const userDestino = await procurarConta(numero_conta_destino, contas);
 
     userOrigem.saldo -= Number(valor);
     userDestino.saldo += Number(valor);
@@ -117,39 +104,39 @@ const transferencia = (req, res) => {
     const data = new Date();
 
     const registro = {
-        "data": format(data, "yyyy-MM-dd HH:mm:ss"),
-        "numero_conta_origem": userOrigem.numero,
-        "numero_conta_destino": userDestino.numero,
-        "valor": Number(valor)
+        data: format(data, "yyyy-MM-dd HH:mm:ss"),
+        numero_conta_origem: userOrigem.numero,
+        numero_conta_destino: userDestino.numero,
+        valor: Number(valor)
     };
 
-    bancoDeDados.transferencias.push(registro);
+    transferencias.push(registro);
 
-    return res.status(200).json({ "mensagem": "Transferência realizada com sucesso!" });
+    return res.status(200).json({ mensagem: "Transferência realizada com sucesso!" });
 };
 
-const saldo = (req, res) => {
+const saldo = async (req, res) => {
     const { numero_conta } = req.query;
 
-    const usuario = bancoDeDados.contas.find((user) => user.numero === numero_conta);
+    const usuario = await procurarConta(numero_conta, contas);
 
-    return res.status(200).json({ "saldo": usuario.saldo });
+    return res.status(200).json({ saldo: usuario.saldo });
 };
 
 const extrato = (req, res) => {
     const { numero_conta } = req.query;
 
-    const registroDeposito = bancoDeDados.depositos.filter((user) => user.numero_conta === numero_conta);
-    const registroSaque = bancoDeDados.saques.filter((user) => user.numero_conta === numero_conta);
-    const arrTransferencias = bancoDeDados.transferencias
+    const registroDeposito = depositos.filter((user) => user.numero_conta === numero_conta);
+    const registroSaque = saques.filter((user) => user.numero_conta === numero_conta);
+    const arrTransferencias = transferencias
     const enviadas = arrTransferencias.filter((userEnv) => userEnv.numero_conta_origem === numero_conta);
     const recebidas = arrTransferencias.filter((userRec) => userRec.numero_conta_destino === numero_conta);
 
     const registro = {
-        "depositos": registroDeposito,
-        "saques": registroSaque,
-        "transferenciasEnviadas": enviadas,
-        "transferenciasRecebidas": recebidas
+        depositos: registroDeposito,
+        saques: registroSaque,
+        transferenciasEnviadas: enviadas,
+        transferenciasRecebidas: recebidas
     };
 
     return res.status(200).json(registro);
